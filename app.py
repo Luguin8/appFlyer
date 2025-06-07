f694a20a4f744d128ba914c4eec0c7810207c030
Luguin <106591394+Luguin8@users.noreply.github.com>
2025-06-07 11:42:23 -0300

    contraseña

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 UPLOAD_FOLDER = 'static'
 FLYER_FILENAME = 'flyer.jpg'
+
+users = {
+    "admin": generate_password_hash("tu_contraseña_segura")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users[username], password):
+        return username
 
 @app.route('/')
 def index():
     return render_template('index.html', flyer_url=url_for('static', filename=FLYER_FILENAME))
 
 @app.route('/admin', methods=['GET', 'POST'])
+@auth.login_required
 def admin():
     if request.method == 'POST':
         flyer = request.files['flyer']
